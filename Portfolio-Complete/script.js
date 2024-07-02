20ae529ade40477d88b5a8b3aa122ab62233bc81
Harsh <hs5686584@gmail.com>
2024-07-02 23:42:54 +0530

    updated portfolio

--- Portfolio-Complete/script.js
+++ Portfolio-Complete/script.js
@@ -1,1 +1,9 @@
-console.log("bye world");
+let p = fetch("https://binaryjazz.us/wp-json/genrenator/v1/genre/");
+
+p.then((response) => {
+  console.log(response.status);
+  console.log(response.ok);
+  return response.json();
+}).then((values) => {
+  console.log(values);
+});
